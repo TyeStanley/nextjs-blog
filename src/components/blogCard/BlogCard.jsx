71734003c88f1b23748de2552314803968a90a0c
Tye Stanley <tyestanley090@gmail.com>
2023-06-17 20:18:07 -0700

    added jsx content for blogcard component + erased page css default css code from next.js

--- src/components/blogCard/BlogCard.jsx
+++ src/components/blogCard/BlogCard.jsx
@@ -1,6 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import classes from "./blogCard.module.css";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 
-export default function BlogCard() {
-  return <div>BlogCard</div>;
+export default function BlogCard({ blog: { title, desc, img } }) {
+  const isLiked = true;
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <Link
+          className={classes.imgContainer}
+          href="/"
+        >
+          <Image
+            src={img}
+            width="350"
+            height="350"
+            alt="temp"
+          />
+        </Link>
+        <div className={classes.blogData}>
+          <div className={classes.left}>
+            <h3>{title}</h3>
+            <p>{desc}</p>
+          </div>
+          <div className={classes.right}>
+            {12}{" "}
+            {isLiked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
