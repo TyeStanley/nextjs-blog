b20f33659912d3172098901492f041f4b2473256
Tye Stanley <tyestanley090@gmail.com>
2023-06-18 19:31:24 -0700

    added post route for /api/blog

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -1,5 +1,5 @@
 import db from "@/lib/db";
-import { verifyToken } from "@/lib/jwt";
+import { verifyJwtToken } from "@/lib/jwt";
 import Blog from "@/models/Blog";
 
 export async function GET(req) {
@@ -13,4 +13,28 @@ export async function GET(req) {
   }
 }
 
-export async function POST(req) {}
+export async function POST(req) {
+  await db.connect();
+
+  // in node.js -> req.headers.authorization
+  const accessToken = req.headers.get("authorization");
+  const token = accessToken.split(" ")[1];
+
+  const decodedToken = verifyJwtToken(token);
+
+  if (!accessToken || !decodedToken) {
+    return new Response(
+      JSON.stringify({ error: "unauthorized (wrong or expired token)" }),
+      { status: 403 }
+    );
+  }
+
+  try {
+    const body = await req.json(body);
+    const newBlog = await Blog.create(body);
+
+    return new Response(JSON.stringify(newBlog), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
