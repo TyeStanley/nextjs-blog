b4d604c481d07e4e3790780c1b7d81a5e480ac84
Tye Stanley <tyestanley090@gmail.com>
2023-06-19 14:36:29 -0700

    added DELETE route for /api/comment/[id] for deleting comments

--- src/app/api/comment/[id]/route.js
+++ src/app/api/comment/[id]/route.js
@@ -16,4 +16,38 @@ export async function GET(req, ctx) {
   }
 }
 
-export async function DELETE(req) {}
+export async function DELETE(req) {
+  await db.connect();
+
+  const id = ctx.params.id;
+  const accessToken = req.headers.get("authorization");
+  const token = accessToken.split(" ")[1];
+
+  const decodedToken = verifyJwtToken(token);
+
+  if (!accessToken || !decodedToken) {
+    return new Response(
+      JSON.stringify({ error: "unauthorized (wrong or expired token)" }),
+      { status: 403 }
+    );
+  }
+
+  try {
+    const comment = await Comment.findById(id).populate("authorId");
+    if (comment.authorId._id.toString() !== decodedToken._id.toString()) {
+      return new Response(
+        JSON.stringify({ msg: "Only author can delete the blog" }),
+        { status: 401 }
+      );
+    }
+
+    await Comment.findByIdAndDelete(id);
+
+    return new Response(
+      JSON.stringify({ msg: "Successfully deleted comment" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
