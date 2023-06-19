5c6f539533b5f71b4382bb034f9cf64016756654
Tye Stanley <tyestanley090@gmail.com>
2023-06-19 10:41:43 -0700

    added DELETE route for /api/blog

--- src/app/api/blog/[id]/route.js
+++ src/app/api/blog/[id]/route.js
@@ -58,4 +58,39 @@ export async function PUT(req, ctx) {
   }
 }
 
-export async function DELETE(req) {}
+export async function DELETE(req, ctx) {
+  await db.connect();
+
+  const id = ctx.params.id;
+
+  const accessToken = req.headers.get("authorization");
+  const token = accessToken.split(" ")[1];
+
+  const decodedToken = verifyJwtToken(token);
+
+  if (!accessToken || !decodedToken) {
+    return new Response(
+      JSON.stringify({ error: "unauthorized (wrong or expired token)" }),
+      { status: 403 }
+    );
+  }
+
+  try {
+    const blog = await Blog.findById(id).populate("authorId");
+
+    if (blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+      return new Response(
+        JSON.stringify({ msg: "Only author can delete the blog" }),
+        { status: 403 }
+      );
+    }
+
+    await Blog.findByIdAndDelete(id);
+
+    return new Response(JSON.stringify({ msg: "Successfully deleted blog" }), {
+      status: 200
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
