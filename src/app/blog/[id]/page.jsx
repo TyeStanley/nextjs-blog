0317b7f2d6e818d3ef69835861ea0dcdc6483122
Tye Stanley <tyestanley090@gmail.com>
2023-06-23 16:42:30 -0700

    added jsx and logic for blog/[id] page

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import classes from "./blog.module.css";
+import { BsFillPencilFill } from "react-icons/bs";
+import { AiFillDelete, AiFillLike, AiOutlineLike } from "react-icons/ai";
+import Link from "next/link";
+import { format } from "timeago.js";
+
+export default function BlogDetails(ctx) {
+  const [blogDetails, setBlogDetails] = useState("");
+  const [isLiked, setIsLiked] = useState(false);
+  const [blogLikes, setBlogLikes] = useState(0);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    async function fetchBlog() {
+      const res = await fetch(`http://localhost:3000/api/blog/${ctx.params.id}`, { cache: "no-store" });
+      const blog = await res.json();
+
+      setBlogDetails(blog);
+      setIsLiked(blog?.likes?.includes(session?.user?._id));
+      setBlogLikes(blog?.likes?.length);
+    }
+    session && fetchBlog();
+  }, [session, ctx.params.id]);
+
+  async function handleDelete() {}
+
+  async function handleLike() {}
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <Image
+          src={blogDetails?.imageUrl}
+          width="750"
+          height="650"
+          alt="blog image"
+        />
+        <div className={classes.row}>
+          <h3 className={classes.title}>{blogDetails?.title}</h3>
+          {blogDetails?.authorId?._id.toString() === session?.user?._id.toString() ? (
+            <div className={classes.controls}>
+              <Link
+                className={classes.editButton}
+                href={`/blog/edit/${ctx.params.id}`}
+              >
+                Edit <BsFillPencilFill />
+              </Link>
+              <button
+                onClick={handleDelete}
+                className={classes.deleteButton}
+              >
+                Delete
+                <AiFillDelete />
+              </button>
+            </div>
+          ) : (
+            <div className={classes.author}>
+              Author: <span>{blogDetails?.authorId?.username}</span>
+            </div>
+          )}
+        </div>
+        <div className={classes.row}>
+          <div className={classes.category}>
+            Category:
+            <span>{blogDetails?.category}</span>
+          </div>
+          <div className={classes.right}>
+            {blogLikes}{" "}
+            {isLiked ? (
+              <AiFillLike
+                size={20}
+                onClick={handleLike}
+              />
+            ) : (
+              <AiOutlineLike
+                size={20}
+                onClick={handleLike}
+              />
+            )}
+          </div>
+        </div>
+        <div className={classes.row}>
+          <p>{blogDetails?.desc}</p>
+          <span>
+            Posted: <span>{format(blogDetails?.createdAt)}</span>
+          </span>
+        </div>
+        <div className={classes.commentSection}>
+          <div className={classes.commentInput}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
