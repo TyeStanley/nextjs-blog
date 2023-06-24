0090242ddb52aeabe6d31ea1a7eac08c4ccc6cdf
Tye Stanley <tyestanley090@gmail.com>
2023-06-23 19:12:09 -0700

    adjusted homepage for blogs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,17 +3,29 @@ import { blogs } from "@/lib/data";
 import Image from "next/image";
 import classes from "./page.module.css";
 
-export default function Home() {
+export async function fetchBlogs() {
+  const res = await fetch("http://localhost:3000/api/blog", { cache: "no-store" });
+
+  return res.json();
+}
+
+export default async function Home() {
+  const blogs = await fetchBlogs();
+
   return (
     <div className={classes.container}>
-      <h2>My Blog Website</h2>
+      {blogs?.length > 0 && <h2>My Blog Website</h2>}
       <div className={classes.wrapper}>
-        {blogs.map(blog => (
-          <BlogCard
-            key={blog.title}
-            blog={blog}
-          />
-        ))}
+        {blogs?.length > 0 ? (
+          blogs.map(blog => (
+            <BlogCard
+              key={blog._id}
+              blog={blog}
+            />
+          ))
+        ) : (
+          <h3 className={classes.noBlogs}>No blogs are currently posted</h3>
+        )}
       </div>
     </div>
   );
