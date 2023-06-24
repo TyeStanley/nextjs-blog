5f14d3c53ac4b99fcb6cd1c8e74f22ea28f8d6dc
Tye Stanley <tyestanley090@gmail.com>
2023-06-24 10:58:31 -0700

    updated comment component

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,6 +1,55 @@
 import React from "react";
+import { useSession } from "next-auth/react";
+import { format } from "timeago.js";
+import Image from "next/image";
 import classes from "./comment.module.css";
+import { BsTrash } from "react-icons/bs";
 
-export default function Comment() {
-  return <div>Comment</div>;
+export default function Comment({ comment, setComments }) {
+  const { data: session } = useSession();
+  const token = session?.user?.accessToken;
+
+  async function handleDeleteComment() {
+    try {
+      await fetch(`http://localhost:3000/api/comment/${comment?._id}`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        },
+        method: "DELETE"
+      });
+
+      setComments(prev => {
+        return [...prev].filter(c => c?._id !== comment?._id);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Image
+            src={person}
+            width="45"
+            height="45"
+            alt="person"
+          />
+          <div className={classes.userData}>
+            <h4>{comment?.authorId?.username}</h4>
+            <span className={classes.timeago}>{format(comment?.createdAt)}</span>
+          </div>
+          <span>{comment?.text}</span>
+        </div>
+        <div className={classes.right}>
+          {session?.user?._id === comment?.authorId?._id && (
+            <BsTrash
+              className={classes.trashIcon}
+              onClick={handleDeleteComment}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
